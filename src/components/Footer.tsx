e1ea743e2f122e2eec9efb9836c5420b62681d97
Sathyajit Loganathan <loganathansathyajit@gmail.com>
2024-06-17 19:15:46 -0600

    Update product name on footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
   const router = useRouter();
   return (
     <div className="flex flex-row justify-center w-screen p-3 mt-2 items-center border-t-[1.5px] custom-border-color gap-4">
-      Copyrights Reserved by STORENAV
+      Copyrights Reserved by nfc-herd
       {/* <Image
         className="hover:cursor-pointer"
         src="/LankaWikiShortBlackLogo.png"
